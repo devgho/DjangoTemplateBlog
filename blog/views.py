44ed392051615fd023af483bd056cfb738752555
devgho <devgho@outlook.com>
2021-06-11 22:09:33 +0800

    My work for the end of this term

---
+++ blog/views.py
@@ -0,0 +1,47 @@
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import DetailView, ListView
+
+from pure_pagination.mixins import PaginationMixin
+
+from .models import Category, Post, Tag
+
+
+class IndexView(PaginationMixin, ListView):
+    model = Post
+    template_name = "blog/index.html"
+    context_object_name = "post_list"
+    paginate_by = 10
+
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get("pk"))
+        return super().get_queryset().filter(category=cate)
+
+
+class ArchiveView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get("year")
+        month = self.kwargs.get("month")
+        return (
+            super()
+            .get_queryset()
+            .filter(created_time__year=year, created_time__month=month)
+        )
+
+
+class TagView(IndexView):
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get("pk"))
+        return super().get_queryset().filter(tags=t)
+
+
+# 记得在顶部导入 DetailView
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = "blog/detail.html"
+    context_object_name = "post"
+
